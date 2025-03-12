f34feae3d0fe58587fa9a5fd906e8393864fbaed
khj82381425 <87836028+pjh82381425@users.noreply.github.com>
2025-03-12 19:35:48 +0900

    edit js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/user/:id', (req, res) => {
+app.get('/place/:id', (req, res) => {
     const param = req.params
     console.log(param)
     console.log(param.id)
 
-    res.json({'id': param.id})
+    res.json((param.id + " 처리 완료"))
 })
 
 app.listen(port, () => {
